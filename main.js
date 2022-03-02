fb496ed076ba1edc7da3ffda37438f98b476b9d8
zishan344 <zishanahmed344@gmail.com>
2022-03-03 00:01:09 +0600

    done the project

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+const byDefult = () => {
+  const url = `https://api.weatherapi.com/v1/current.json?key=c2332aa5f0464d69a1660750222802&q=Dhaka&aqi=no`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => showLoadData(data));
+};
+byDefult();
+
 const loadApi = () => {
   const searchBox = document.getElementById("search-box");
   const url = `https://api.weatherapi.com/v1/current.json?key=c2332aa5f0464d69a1660750222802&q=${searchBox.value}&aqi=no`;
@@ -6,6 +14,25 @@ const loadApi = () => {
     .then((data) => showLoadData(data));
   searchBox.value = "";
 };
+const setText = (id, text, img = false) => {
+  const setText = document.getElementById(id);
+  if (img == true) {
+    setText.setAttribute("src", text);
+    // setText.setAttribute("src", text);
+    return setText;
+  } else {
+    setText.innerText = text;
+    return setText;
+  }
+};
+
 const showLoadData = (data) => {
-  const searchResults = document.getElementById("weather-card");
+  console.log(data);
+  // console.log(data.condition.icon, false);
+  setText("city-name", data.location.name);
+  setText("weather-img-heding", data.current.condition.icon, true);
+  setText("temp", data.current.temp_c);
+  setText("current-taxt", data.current.condition.text);
+  setText("wind", data.current.wind_kph);
+  setText("hum", data.current.humidity);
 };
